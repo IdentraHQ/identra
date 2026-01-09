0ffa12fed64a48dbc515a76884eb24f47ce55147
keir <darksalterego99@gmail.com>
2026-01-09 11:04:24 +0530

    feat: update libc dependency to version 0.2.180 and refactor global shortcut handler

--- clients/ghost-desktop/src-tauri/src/lib.rs
+++ clients/ghost-desktop/src-tauri/src/lib.rs
@@ -3,11 +3,34 @@ pub mod state;
 
 use crate::commands::*;
 use crate::state::NexusState;
+use tauri::Manager;
+use tauri_plugin_global_shortcut::{Code, Modifiers, ShortcutState, Shortcut};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .manage(NexusState::new())
+        .plugin(
+            tauri_plugin_global_shortcut::Builder::new()
+                .with_shortcut(Shortcut::new(Some(Modifiers::ALT), Code::Space))
+                .unwrap()
+                // CHANGE: .on_shortcut -> .with_handler
+                .with_handler(|app, shortcut, event| {
+                    if event.state == ShortcutState::Pressed {
+                        if shortcut.matches(Modifiers::ALT, Code::Space) {
+                            if let Some(launcher) = app.get_webview_window("launcher") {
+                                if launcher.is_visible().unwrap_or(false) {
+                                    let _ = launcher.hide();
+                                } else {
+                                    let _ = launcher.show();
+                                    let _ = launcher.set_focus();
+                                }
+                            }
+                        }
+                    }
+                })
+                .build(),
+        )
         .invoke_handler(tauri::generate_handler![
             toggle_launcher,
             toggle_main_window,
